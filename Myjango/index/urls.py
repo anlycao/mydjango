16e877066ea36f86c02ee12ba290f36a96bbfb7e
anlycao <736426072@qq.com>
2020-04-01 22:47:51 +0800

    set urls

--- Myjango/index/urls.py
+++ Myjango/index/urls.py
@@ -1,8 +1,11 @@
 
-from django.urls import path # 导入url编写模块
-from index import views
+from django.urls import path, re_path  # 导入url编写模块
+from . import views
 
 
 urlpatterns=[
-    path('',views.index)
+    path('',views.index),
+    #带有字符类型、整型的url
+    #path('<year>/<int:month>/<slug:day>',views.mydate),    #http://127.0.0.1/2020/05/01
+    re_path('(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2}).html',views.mydate)#http://127.0.0.1/2016/15/21.html
 ]
--- Myjango/index/views.py
+++ Myjango/index/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     return HttpResponse("HELLO WORLD")
+def mydate(request,year,month,day):
+    return HttpResponse(str(year)+'/'+str(month)+'/'+str(day))
